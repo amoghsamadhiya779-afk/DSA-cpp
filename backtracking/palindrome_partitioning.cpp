ccf5f2e447804c034a3fde5fcd01745a78431f3d
amoghsamadhiya779-afk <amoghsamadhiya779@gmail.com>
2026-02-14 15:34:50 +0530

    arrays

---
+++ backtracking/palindrome_partitioning.cpp
@@ -0,0 +1,49 @@
+// Given string s partiiton s such that every substring of the partition is a plindrome partitioning
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+    vector<vector<string>>partition(string s){
+        vector<vector<string>> res;
+        vector<string>path;
+        func(0,s,path,res);
+        return res;
+    }
+
+    void func(int index, string s ,vector<string>&path,vector<vector<string>>&res){
+        if(index==s.size()){
+            res.push_back(path);
+            return;
+        }
+
+        for (int i =index;i<s.size();i++){
+            if(isPalindrome(s,index,i)) {
+             path.push_back(s.substr(index,i-index+1));
+             func(i+1,s,path,res);
+             path.pop_back();
+            }
+        }
+    }
+
+    bool isPalindrome(string s,int start,int end){
+        while (start<=end){
+            if (s[start++] != s[end--])
+            return false;
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "aab";
+    vector<vector<string>> result = sol.partition(s);
+    for (const auto& partition : result) {
+        for (const string& str : partition) {
+            cout << str << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
